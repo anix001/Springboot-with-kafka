322779407770e6ee8418aca831c588140bbe670c
anix001 <chaudharyanish305@gmail.com>
2023-05-05 15:19:22 +0545

    Improvement: Rest API to send Json Object

---
+++ src/main/java/com/kafka/resource/JsonMessageResource.java
@@ -0,0 +1,26 @@
+package com.kafka.resource;
+
+import com.kafka.dto.User;
+import com.kafka.kafka.JsonKafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageResource {
+
+    private JsonKafkaProducer jsonKafkaProducer;
+
+    public JsonMessageResource(JsonKafkaProducer jsonKafkaProducer) {
+        this.jsonKafkaProducer = jsonKafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user){
+        jsonKafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Message sent to kafka topic");
+    }
+}
